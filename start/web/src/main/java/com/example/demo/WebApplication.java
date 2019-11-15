ec7cf2ff218bb2cb4234b4e697054281181e2d2c
bicen.zhu <bicen.zhu@nokia-sbell.com>
2019-11-15 16:38:54 +0800

    more

--- start/web/src/main/java/com/example/demo/WebApplication.java
+++ start/web/src/main/java/com/example/demo/WebApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -11,8 +10,6 @@ import org.springframework.cloud.sleuth.Sampler;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
